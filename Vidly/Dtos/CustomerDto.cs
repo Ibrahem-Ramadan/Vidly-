b32947950911a2886d83df2483569568925ccb09
Ibrahem-Ramadan <ibrahem.ramadan@cis.asu.edu.eg>
2021-08-04 14:57:23 +0200

    create Customer DTO

---
+++ Vidly/Dtos/CustomerDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.Dtos
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedtoNewsLetter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+
+        [_18YearsValidationIfAMember]
+        public System.Nullable<DateTime> BirthDate { get; set; }
+    }
+}
